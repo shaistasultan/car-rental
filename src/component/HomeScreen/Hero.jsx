2b3f032d75bd9c3dcb90b46058a7418cca011bef
shaistasultan <shaistasultan.16@gmail.com>
2024-04-18 17:36:49 +0500

    complete home section

--- src/component/HomeScreen/Hero.jsx
+++ src/component/HomeScreen/Hero.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-
+import MainCar from "../../assets/main.jpeg"
+import { FaInstagram } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa";
+import { FaTelegramPlane } from "react-icons/fa";
 function Hero(){
     const NavLinks =[
         {
@@ -31,14 +36,14 @@ function Hero(){
 
     return(
         <>
-       <header >
-          <nav className="bg-slate-400 max-w-[1120px] mx-auto mt-6">
-               <div className="container ">
+       <header className="relative  max-w-[1440px] max-h-[742]" >
+          <nav className="absolute container top-[32px] left-[180px] bg-transparent min-w-[1400px] mx-auto ">
+               <div className=" ">
                   <div className="flex justify-between ">
                        <div className="text-primary py-2 ">
                            <h1 className="text-3xl font-bold font-serif ">Car Rental</h1>
                        </div>
-                       <div className="pl-40">
+                       <div className="pl-96">
                           <ul className="flex items-center gap-8"> 
                           {
                                NavLinks.map((data) =>(
@@ -53,20 +58,51 @@ function Hero(){
                      
   
                         </div>
-                       <div className="pr-4">
-                           <button className="py-2  bg-primary text-White1 border-2 border-primary rounded
+                       <div className="pr-4 py-1">
+                           <button className="py-2  bg-primary text-black border-2 border-primary rounded
                             hover:text-primary hover:bg-White1 transition-colors duration-500 text-lg font-medium">Login | Sign Up</button>
                        </div>
                   
                     </div>
               </div>
          </nav>
-       
-           <div className="container bg-black min-h-[742px] max-w-[613px]">
+           
+           <div className="container bg-black min-h-[1000px] max-w-[742px]">
+              <div className="pt-[300px] pl-[100px]">
+               <div class="h-20 w-0.5 bg-primary mx-5 mb-9" ></div>
+               <FaInstagram className="bg-primary text-2xl  ml-2" />
+               <FaFacebook className="bg-primary text-2xl mt-3 ml-2" />
+               <FaTwitter className="bg-primary text-2xl mt-3 ml-2" />
+               <FaWhatsapp className="bg-primary text-2xl mt-3 ml-2" />
+               <FaTelegramPlane className="bg-primary text-2xl mt-3 ml-2" />
+               <div class="h-20 w-0.5 bg-primary mx-5 mt-9" ></div>
+
+              </div>
 
            </div>
+           <div className="absolute top-[130px] left-[650px]  ">
+            <img src={MainCar} alt=""  className="min-h-[545px] max-w-[1200px]"/>
+           </div>
+
+           <div className="absolute container bg-black bg-opacity-25 max-w-[678px] min-h-[585px] top-[240px] left-[270px] pt-[50px]" >
+
+             <div className="text-white p-7">
+                <h1 className="text-6xl font-serif font-bold">Enjoy your life with our comfortable cars.</h1>
+                <p className="text-4xl font-serif font-bold pt-6">Carent, is ready to serve the best
+                <br/>experience in car rental.</p>
+
+             </div>
+             <div className="ml-[30px]"> 
+                 <button className="py-2  bg-primary text-black border-2 border-primary rounded
+                            hover:text-primary hover:bg-White1 transition-colors duration-500 text-lg font-medium px-8">Explore Now</button>
+            </div>
+                   
+
+           </div>
+
+          
            
-         
+  
         </header>
 
 
